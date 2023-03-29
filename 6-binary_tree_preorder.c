6ff490d9ce716c8ff9502c0a114db6e528f5c2d8
Jojo <jojothomas1515@gmail.com>
2023-03-29 15:36:38 +0100

    task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - operate in the preorder travesal manner
+ * @tree: tree to operate on
+ * @func: function used to operate on the tree
+ *
+ * Description: operate on the value of tree node with with passed function
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}

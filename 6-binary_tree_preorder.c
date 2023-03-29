4e3d94628fdf9ab623702edbcc764fe0260104df
Jojo <jojothomas1515@gmail.com>
2023-03-29 16:39:57 +0100

    task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,10 +10,12 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree)
+	if (tree != NULL)
 	{
 		func(tree->n);
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
+		if (tree->left)
+			binary_tree_preorder(tree->left, func);
+		if (tree->right)
+			binary_tree_preorder(tree->right, func);
 	}
 }

fdd1f4ceb34926e562c8648841200a63b19ad3fa
Joseph Thomas Ehigie <jojothomas1515@gmail.com>
2023-03-30 03:15:26 -0700

    battery low , continue on the other pc

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - check the balance factor of a binary tree
+ * @tree: tree node
+ * Return: balance factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+
+}

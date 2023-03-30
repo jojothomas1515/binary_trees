77c3b4d6e24db412f37b01b37820d435ca7d4d6e
Jojo <jojothomas1515@gmail.com>
2023-03-30 22:31:50 +0100

    tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,7 +9,7 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (1);
+		return (0);
 
 	if ((tree && ((tree->left != NULL && tree->right != NULL) ||
 		      (!tree->right && !tree->left))) ||

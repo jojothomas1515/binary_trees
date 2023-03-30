43acf3cb30d54e143c827d61087b630c645472bf
Jojo <jojothomas1515@gmail.com>
2023-03-30 21:37:33 +0100

    tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+size_t bth(const binary_tree_t *tree);
+
 /**
  * binary_tree_balance - check the balance factor of a binary tree
  * @tree: tree node
@@ -8,15 +10,36 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left;
-	int right;
+	int left = 0;
+	int right = 0;
 
 	if (tree)
 	{
-		left = binary_tree_height(tree->left);
-		right = binary_tree_height(tree->right);
+		left = bth(tree->left);
+		right = bth(tree->right);
 		return (left - right);
 	}
 
+	return (0);
+}
+
+
+/**
+ * bth - get the height of binary tree
+ * @tree: the tree
+ * Return: the height of the node
+ */
+
+size_t bth(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree)
+	{
+	       	left = 1 + bth(tree->left);
+		right = 1 + bth(tree->right);
+		return (left > right ? left : right);
+
+	}
 	return (0);
 }
